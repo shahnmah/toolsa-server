1e4c50b74335c64f360f0105ee3d701a5638ff06
shahnmah <shahn.mah@gmail.com>
2022-05-26 03:32:39 +0600

    app for load all tools

--- index.js
+++ index.js
@@ -9,3 +9,38 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+// connection uri
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster2.ic3nj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const toolCollection = client.db('toolsa').collection('tools');
+        const reviewCollection = client.db('toolsa').collection('reviews');
+
+        // get api for load all tools
+        app.get('/tool', async (req, res) => {
+            const query = {};
+            const cursor = toolCollection.find(query);
+            const tools = await cursor.toArray();
+            res.send(tools);
+          });
+       
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Okay with toolsa')
+})
+
+app.listen(port, () => {
+    console.log(`Toolsa ${port}`)
+})
