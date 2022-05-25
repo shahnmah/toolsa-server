f857393574af76c27244698b08d93c45a497d3bc
shahnmah <shahn.mah@gmail.com>
2022-05-26 02:30:06 +0600

    'initialized'

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
