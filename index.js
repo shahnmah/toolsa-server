17eb05494d749042d401aadbf61b3da28c787925
shahnmah <shahn.mah@gmail.com>
2022-05-27 21:36:32 +0600

    add review api added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,45 +15,51 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster2.ic3nj.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
-    try {
-        await client.connect();
-        const toolCollection = client.db('toolCollection').collection('tools');
-        const reviewCollection = client.db('reviewCollection').collection('reviews');
+  try {
+    await client.connect();
+    const toolCollection = client.db('toolCollection').collection('tools');
+    const reviewCollection = client.db('reviewCollection').collection('reviews');
 
-        // get api for load all tools
-        app.get('/tool', async (req, res) => {
-            const query = {};
-            const cursor = toolCollection.find(query);
-            const tools = await cursor.toArray();
-            res.send(tools);
-          });
-        // get api for load all reviews
-        app.get('/review', async (req, res) => {
-            const query = {};
-            const cursor = reviewCollection.find(query);
-            const reviews = await cursor.toArray();
-            res.send(reviews);
-          });
+    // get api for load all tools
+    app.get('/tool', async (req, res) => {
+      const query = {};
+      const cursor = toolCollection.find(query);
+      const tools = await cursor.toArray();
+      res.send(tools);
+    });
+    // get api for load all reviews
+    app.get('/review', async (req, res) => {
+      const query = {};
+      const cursor = reviewCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
 
-        //   load tool data using id
-        app.get('/buyNow/:id', async(req, res)=>{
-            const id = req.params.id;
-            const query= {_id: ObjectId(id)}
-            const tool = await busCollection.findOne(query)
-            res.send(tool)
-          })
-    }
-    finally {
+    //   load tool data using id
+    app.get('/buyNow/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
+      const tool = await toolCollection.findOne(query)
+      res.send(tool)
+    })
+    // add review api
+    app.post('/addreview', async (req, res) => {
+      const data = req.body;
+      const newReview = await reviewCollection.insertOne(data)
+      res.send(newReview)
+    })
+  }
+  finally {
 
-    }
+  }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Okay with toolsa')
+  res.send('Okay with toolsa')
 })
 
 app.listen(port, () => {
-    console.log(`Toolsa ${port}`)
+  console.log(`Toolsa ${port}`)
 })
